80cdfa057d0371c17bd2d8c53a632036860ee7c0
isaakavo <isaakhaas96@gmail.com>
2023-03-14 21:34:13 -0600

    feat: first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Heading } from '@chakra-ui/react';
+
+function App() {
+  return (
+    <div className='App'>
+      <Heading>Cenoteando Frontend</Heading>
+    </div>
+  );
+}
+
+export default App;
