fd5e2fcc3057505724f100c6e0c33964bd458d7b
isaakavo <isaakhaas96@gmail.com>
2023-03-21 22:12:02 -0600

    feat: styles and redirection on navbar

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,18 +1,52 @@
 import React from 'react';
 
-import { Flex, Heading, Spacer, ButtonGroup, Button, Box } from '@chakra-ui/react';
+import logo from '../../../public/logo.png';
+
+import {
+  Flex,
+  Spacer,
+  ButtonGroup,
+  Button,
+  Box,
+  Image,
+  Text,
+  Heading,
+} from '@chakra-ui/react';
+import { Link } from 'react-router-dom';
 
 export const NavBar = () => {
   return (
-    <Flex minWidth='max-content' alignItems='center' gap='2'>
-      <Box p='2'>
-        <Heading size='md'>Chakra App</Heading>
-      </Box>
-      <Spacer />
-      <ButtonGroup gap='2'>
-        <Button colorScheme='teal'>Sign Up</Button>
-        <Button colorScheme='teal'>Log in</Button>
-      </ButtonGroup>
+    <Flex
+      minWidth='max-content'
+      alignItems='center'
+      gap='2'
+      justifyContent='space-around'
+      bg='Highlight'
+      mb='20px'
+    >
+      <Link to='/'>
+        <Flex
+          p='2'
+          flexDirection='row'
+          alignSelf='center'
+          alignItems='center'
+          ml='10px'
+        >
+          <Image
+            boxSize='50px'
+            objectFit='cover'
+            alt='Cenoteando-logo'
+            src={logo}
+          />
+          <Heading size='sm' ml='12px'>
+            Cenoteando
+          </Heading>
+        </Flex>
+      </Link>
+      <Flex gap='2' alignItems='center'>
+        <Link to='admin/cenotes'>Admin</Link>
+        <Link to='admin/variables'>Variables</Link>
+      </Flex>
     </Flex>
   );
 };
