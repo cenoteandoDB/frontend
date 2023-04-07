2a08328bc02e75f03f1e3f9eeeab5a9b2e60d817
isaakavo <isaakhaas96@gmail.com>
2023-04-06 22:29:38 -0600

    feat: change of enum values

--- src/models/CenotesTypes.ts
+++ src/models/CenotesTypes.ts
@@ -15,11 +15,11 @@ export interface geoJsonI {
 }
 
 export enum CenoteType {
-  NO_TYPE = 'NO_TYPE',
+  NO_TYPE = 'Sin tipo',
   CENOTE = 'CENOTE',
-  DRY_CAVE = 'DRY_CAVE',
-  WATER_WELL = 'WATER_WELL',
-  WATERY = 'WATERY',
+  DRY_CAVE = 'Cueva seca',
+  WATER_WELL = 'Pozo',
+  WATERY = 'Acuoso',
 }
 
 export enum CenoteIssue {
@@ -50,7 +50,7 @@ export class CenoteModel {
   constructor(jsonObj?: CenoteModel) {
     if (jsonObj) {
       this.id = jsonObj.id;
-      this.type = jsonObj.type;
+      this.type = CenoteType[jsonObj.type as keyof typeof CenoteType];
       this.name = jsonObj.name;
       this.touristic = jsonObj.touristic;
       this.issues = jsonObj.issues;
