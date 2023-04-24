3adc6e75685cfa921dff3657dbf43f029a9fae28
isaakavo <isaakhaas96@gmail.com>
2023-04-23 20:45:47 -0600

    asdasd

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Center, Heading } from '@chakra-ui/react';
+import { Center } from '@chakra-ui/react';
 import { Outlet } from 'react-router-dom';
 
 export const Home = () => {
