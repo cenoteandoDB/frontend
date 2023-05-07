bb4d99670e428a2ff6c8b1e34b20b21e092a6549
Isaak Haas <isaakhaas96@gmail.com>
2023-05-06 20:07:20 -0600

    Firebase (#11)
    * feat: adding remaining columns for variables
    
    * feat: adding firebase
    
    * fix: ts issue
    
    * feat: add firebase.json
    
    * feat: adding dev env variables
    
    * fix: fix of headers
    
    * chore: clean code
    
    * feat: adding dev env variable
    
    * fix: fix of env variable data
    
    * fix: removing unnecesary code

--- src/services/http-client.ts
+++ src/services/http-client.ts
@@ -1,11 +1,9 @@
 export const httpClient = {
-  baseURL: 'http://localhost:8080/',
+  baseURL: import.meta.env.VITE_CLIENT_URL,
   timeout: 10000,
   headers: {
     post: {
       'content-Type': 'application/json',
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
     },
   },
 };
