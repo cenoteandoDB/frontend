8f4f4bea0d39be8f49a56f7e32e7e31dc43841c2
isaakavo <isaakhaas96@gmail.com>
2023-05-30 21:01:29 -0600

    feat: image component

--- src/pages/cenote/components/cenote-gallery/cenote-gallery.tsx
+++ src/pages/cenote/components/cenote-gallery/cenote-gallery.tsx
@@ -1,25 +1,78 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import { AspectRatio, Box, IconButton, Image } from '@chakra-ui/react';
+import {
+  AspectRatio,
+  Box,
+  Flex,
+  IconButton,
+  Image,
+  Text,
+} from '@chakra-ui/react';
 import image1 from '../../../../../public/Kankirixché011.jpeg';
 import image2 from '../../../../../public/Kankirixché012.jpeg';
 import { ChevronLeftIcon, ChevronRightIcon } from '@chakra-ui/icons';
 
 export const CenoteGallery: React.FC = () => {
+  const [currentImage, setCurrentImage] = useState(0);
   const imageArr = [image1, image2, image1, image2];
+  const imagSize = imageArr.length;
+
+  const prevSlide = () => {
+    setCurrentImage((s) => (s === 0 ? imagSize - 1 : s - 1));
+  };
+
+  const nextSlide = () => {
+    setCurrentImage((s) => (s === imagSize - 1 ? 0 : s + 1));
+  };
   return (
-    <Box display='flex' gap='15px' alignItems='center' overflow='hidden'>
-      <IconButton aria-label='' size='lg'>
+    <Box
+      display='flex'
+      w='full'
+      overflow='hidden'
+      pos='relative'
+      alignItems='center'
+    >
+      <Flex
+        h='400px'
+        w='full'
+        transition='all 0.5s'
+        ml={`-${currentImage * 100}%`}
+      >
+        {imageArr.map((img, index) => (
+          <Box key={index} boxSize='full' flex='none'>
+            <Text
+              color='white'
+              fontSize='xs'
+              p='8px 12px'
+              pos='absolute'
+              top='0'
+            >
+              {index + 1} / {imagSize}
+            </Text>
+            <Image src={img} boxSize='full' backgroundSize='cover' />
+          </Box>
+        ))}
+      </Flex>
+      <IconButton
+        aria-label=''
+        size='lg'
+        onClick={prevSlide}
+        pos='absolute'
+        w='auto'
+        transition='0.6s ease'
+        left={0}
+      >
         <ChevronLeftIcon />
       </IconButton>
-      <Box display='flex' minW='md' gap='15px'>
-        {imageArr.map((img, index) => (
-          <AspectRatio key={`${img}-${index}`} ratio={4 / 3} minWidth='md' overflow='hidden'>
-            <Image src={img} objectFit='cover' />
-          </AspectRatio>
-        ))}
-      </Box>
-      <IconButton aria-label='' size='lg'>
+      <IconButton
+        aria-label=''
+        size='lg'
+        onClick={nextSlide}
+        pos='absolute'
+        w='auto'
+        transition='0.6s ease'
+        right={0}
+      >
         <ChevronRightIcon />
       </IconButton>
     </Box>
